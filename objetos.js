73945b8bf9adb845e3fde03f772fd01e4bc02360
alexxcalle <a.xavier.c@hotmail.com>
2023-11-30 09:52:12 -0500

    primeraClase

---
+++ objetos.js
@@ -0,0 +1,18 @@
+"use strict"
+
+function Jugador(nickname, vidas, cartas){
+    this.nickname=nickname;
+    this.vidas=vidas;
+    this.cartas=cartas;
+
+}
+
+let jugadorUno = new Jugador("Juan", 3, 7);
+//interactuar html
+let labelPUno = document.getElementById("nick-playerUno");
+labelPUno.innerHTML = `Nick J1: ${jugadorUno.nickname}`;
+
+let jugadorDos = new Jugador("Carlos", 3, 10);
+//interactuar html
+let labelPDos = document.getElementById("nick-playerDos");
+labelPDos.innerHTML = `Nick J1: ${jugadorDos.nickname}`;
